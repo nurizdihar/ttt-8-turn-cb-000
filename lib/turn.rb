6632db5b127afff063bc1963092b52ba2546f577
Nur Izdihar Binte Ismail <nurizdiharismail@gmail.com>
2017-09-11 10:41:25 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,31 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if !position_taken?(board, index) && 0 <= index && 8 >= index
+    return true
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  elsif board[index] == "" || board[index] == " " || board[index] == "nil"
+    return false
+  end
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
+
+def input_to_index(input)
+  number = input.to_i
+  index = number - 1
+  return index
+end
